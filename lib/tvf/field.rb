7e9b6fb4319c073fc9c76d423163025f4910711b
jgpawletko <jgpawletko@gmail.com>
2016-04-13 20:59:41 -0400

    add initial Field#valid?

--- lib/tvf/field.rb
+++ lib/tvf/field.rb
@@ -1,4 +1,30 @@
 module TVF
   class Field
+
+    #    ATTRIBUTES = [ :data_type, :facetable, :indexed, :mandatory, :multiple, :searchable, :stored ]
+    #    attr_reader *ATTRIBUTES
+    attr_reader :data_type, :facetable, :indexed, :mandatory, :multiple, :searchable, :stored
+
+    def initialize(args)
+      # stupid implementation for now.
+      @data_type = args[:data_type]
+      @facetable = args[:facetable]
+      @indexed = args[:indexed]
+      @mandatory = args[:mandatory]
+      @multiple  = args[:multiple]
+      @searchable = args[:searchable]
+      @stored = args[:stored]
+    end
+
+    # this can be cleaned up
+    # again, starting with naive implementation
+    def valid?
+      result = true
+      result &&= %w(text integer).include? data_type
+      [:facetable, :indexed, :mandatory, :multiple, :searchable, :stored].each do |k|
+        result &&= ([true, false].include? self.send(k))
+      end
+      result
+    end
   end
 end
--- spec/field_spec.rb
+++ spec/field_spec.rb
@@ -1,7 +1,32 @@
 require 'spec_helper'
 module TVF
   describe Field do
-    subject(:field) { Field.new }
-    it { should be_an TVF::Field }
+    let(:valid_args) do
+      { data_type: 'integer',
+        facetable: true,
+        indexed: true,
+        mandatory: true,
+        multiple: false,
+        searchable: false,
+        stored: true }
+    end
+
+    let(:invalid_args) do
+      x = valid_args.clone
+      x[:mandatory] = 'hahaha'
+      x
+    end
+
+    describe '#valid' do
+      context 'with valid arguments' do
+        subject { Field.new(valid_args).valid? }
+        it { should be true }
+      end
+
+      context 'with invalid arguments' do
+        subject { Field.new(invalid_args).valid? }
+        it { should_not be true }
+      end
+    end
   end
 end
