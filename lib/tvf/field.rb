6721fc6f0c12be21386301765865f007aabbc060
jgpawletko <jgpawletko@gmail.com>
2016-04-13 21:48:34 -0400

    refactor Field

--- lib/tvf/field.rb
+++ lib/tvf/field.rb
@@ -1,9 +1,13 @@
 module TVF
   class Field
 
-    #    ATTRIBUTES = [ :data_type, :facetable, :indexed, :mandatory, :multiple, :searchable, :stored ]
-    #    attr_reader *ATTRIBUTES
-    attr_reader :data_type, :facetable, :indexed, :mandatory, :multiple, :searchable, :stored
+    VALID_DATA_TYPES = %w(text integer).freeze
+    DATA_TYPE_ATTRIBUTES = [:data_type].freeze
+    BOOLEAN_ATTRIBUTES = [:facetable, :indexed, :mandatory, :multiple,
+                          :searchable, :stored].freeze
+    ALL_ATTRIBUTES = DATA_TYPE_ATTRIBUTES + BOOLEAN_ATTRIBUTES
+
+    attr_reader *ALL_ATTRIBUTES
 
     def initialize(args)
       # stupid implementation for now.
@@ -16,14 +20,10 @@ module TVF
       @stored = args[:stored]
     end
 
-    # this can be cleaned up
-    # again, starting with naive implementation
     def valid?
       result = true
-      result &&= %w(text integer).include? data_type
-      [:facetable, :indexed, :mandatory, :multiple, :searchable, :stored].each do |k|
-        result &&= ([true, false].include? self.send(k))
-      end
+      DATA_TYPE_ATTRIBUTES.each { |k| result &&= VALID_DATA_TYPES.include? self.send(k) }
+      BOOLEAN_ATTRIBUTES.each { |k| result &&= [true, false].include? self.send(k) }
       result
     end
   end
