a99e6a83a4f1b3027da5e4f6d793622bc428a76b
jgpawletko <jgpawletko@gmail.com>
2016-04-13 22:02:37 -0400

    add comments

--- lib/tvf/field.rb
+++ lib/tvf/field.rb
@@ -1,4 +1,5 @@
 module TVF
+  # class stores field attributes
   class Field
 
     VALID_DATA_TYPES = %w(text integer).freeze
@@ -7,9 +8,11 @@ module TVF
                           :searchable, :stored].freeze
     ALL_ATTRIBUTES = DATA_TYPE_ATTRIBUTES + BOOLEAN_ATTRIBUTES
 
+    # make all attributes accessible, but read-only
     attr_reader *ALL_ATTRIBUTES
 
     def initialize(args)
+      # set all attribute instance variables
       ALL_ATTRIBUTES.each { |attr| instance_variable_set("@#{attr}", args[attr]) }
     end
 
