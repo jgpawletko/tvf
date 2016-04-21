9f1c4efe38f49f0dea45117703fed5785c74c97b
jgpawletko <jgpawletko@gmail.com>
2016-04-21 09:16:44 -0400

    add specs, fixtures, Terms#vocabularies

--- lib/tvf/terms.rb
+++ lib/tvf/terms.rb
@@ -1,4 +1,24 @@
 module TVF
   class Terms
+    attr_reader :vocabs
+    private :vocabs
+
+    def initialize(args)
+      @vocabs = init_vocabularies(args)
+    end
+
+    def vocabularies
+      vocabs.keys
+    end
+
+    private
+
+    def init_vocabularies(arg)
+      hash = {}
+      arg.each_pair do |k, v|
+        hash[k] = Vocabulary.new(v)
+      end
+      hash
+    end
   end
 end
--- spec/terms_spec.rb
+++ spec/terms_spec.rb
@@ -1,7 +1,13 @@
 require 'spec_helper'
+require 'yaml'
+
 module TVF
   describe Terms do
-    subject(:terms) { Terms.new }
-    it { should be_an TVF::Terms }
+    let(:valid_args) { Utils.symbolize_keys(YAML.load_file('spec/fixtures/metadata_fields.yml'))[:terms][:vocabulary] }
+
+    describe '#vocabularies' do
+      subject { Terms.new(valid_args).vocabularies }
+      it { should == [:dcterms, :ichabod, :nyucore] }
+    end
   end
 end
